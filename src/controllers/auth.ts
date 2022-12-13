88a52f951b7d705b69aed5e719b892f512d87d72
Man Tran <tranduyminhman@gmail.com>
2022-12-13 10:24:50 +0700

    wip role

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,31 +1,49 @@
 import { NextFunction, Request, response, Response } from "express";
 import Controller from "../decorators/controller";
-import { Get, Post } from "../decorators/handlers";
+import { Post } from "../decorators/handlers";
+import { UserRoles } from "../utils/enums";
 import { BadRequestError } from "../utils/errors";
 
 @Controller("/")
 export default class AuthController {
-  @Get("/session")
-  public getSession(req: Request, res: Response, next: NextFunction): void {
-    res.locals.data = { foo: "bar" };
+  @Post("/session")
+  public session(req: Request, res: Response, next: NextFunction): void {
+    res.locals.data = req.body;
     next();
   }
 
-  @Post("/create-account")
-  public createAccount(req: Request, res: Response, next: NextFunction): void {
-    const { email, password } = req.body;
-    if (!(email && password)) {
-      throw new BadRequestError();
+  @Post("/register")
+  public async register(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { idToken, email, password, role = UserRoles.USER } = req.body;
+      const { admin } = req.app.locals;
+
+      if (!Object.values(UserRoles).includes(role)) {
+        throw new BadRequestError("Invalid role!");
+      }
+      let uid;
+      if (idToken) {
+        const claims = await admin.auth().verifyIdToken(idToken);
+        await admin.auth().setCustomUserClaims(claims.sub, { role });
+        uid = claims.sub;
+      } else {
+        if (!(email && password)) {
+          throw new BadRequestError();
+        }
+        const user = await admin.auth().createUser(req.body);
+        await admin.auth().setCustomUserClaims(user.uid, { role });
+        uid = user.uid;
+      }
+      res.locals.data = {
+        user: await admin.auth().getUser(uid),
+      };
+      next();
+    } catch (error) {
+      next(error);
     }
-    req.app.locals.admin
-      .auth()
-      .createUser(req.body)
-      .then((response: any) => {
-        res.locals.data = response;
-        next();
-      })
-      .catch((error: any) => {
-        next(error);
-      });
   }
 }
