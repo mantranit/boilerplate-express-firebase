459f5675f4a4ad5c4d9ac5c70c4d34f60c2520fd
Man Tran <tranduyminhman@gmail.com>
2024-07-30 10:46:27 +0700

    update config

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,16 +1,35 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import config from "../utils/config";
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: config.dbHost,
-  port: config.dbPort,
-  username: config.dbUsername,
-  password: config.dbPassword,
-  database: config.dbDatabase,
-  synchronize: config.dbSynchronize,
-  logging: config.dbLogging,
-  entities: ["src/database/entities/*.ts"],
-  subscribers: ["src/database/subscribers/*.ts"],
-  migrations: ["src/database/migrations/*.ts"],
-});
+function getConfig(): DataSourceOptions {
+  if (config.env !== "development") {
+    return {
+      type: "postgres",
+      host: config.dbHost,
+      port: config.dbPort,
+      username: config.dbUsername,
+      password: config.dbPassword,
+      database: config.dbDatabase,
+      synchronize: config.dbSynchronize,
+      logging: config.dbLogging,
+      entities: [__dirname + "/../**/entities/*.js"],
+      subscribers: [__dirname + "/../**/subscribers/*.js"],
+      migrations: [__dirname + "/../**/migrations/*.js"],
+    };
+  }
+  return {
+    type: "postgres",
+    host: config.dbHost,
+    port: config.dbPort,
+    username: config.dbUsername,
+    password: config.dbPassword,
+    database: config.dbDatabase,
+    synchronize: config.dbSynchronize,
+    logging: config.dbLogging,
+    entities: ["src/database/entities/*.ts"],
+    subscribers: ["src/database/subscribers/*.ts"],
+    migrations: ["src/database/migrations/*.ts"],
+  };
+}
+
+export const AppDataSource = new DataSource(getConfig());
